b84477608c7a4a4779e7c450fc509d7d4c4ad3b2
kevin hendrieckx <s129200@ap.be>
2022-04-27 22:24:12 +0200

    quiz

--- Project/index.ts
+++ Project/index.ts
@@ -11,19 +11,7 @@ app.get("/index", (req: any, res: any) => {
   res.render("index", {});
 });
 
-<<<<<<< HEAD
 app.listen(app.get("port"), () =>
   console.log("[server] http://localhost:" + app.get("port") + "/index")
 );
 export {};
-=======
-app.get('/landing',(req:any,res:any)=>{
-    
-    res.render('landing',{});
-    
-});
-
-
-app.listen(app.get('port'), ()=>console.log( '[server] http://localhost:' + app.get('port') + '/landing'));
-export{}
->>>>>>> 17c4b3dc21c6183c7c77c61ea16be036a440f277
